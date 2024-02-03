3e4590b427ce8915b1079dd1582687af1ce3a156
rafaellore <birdneon3@gmail.com>
2024-02-03 19:06:01 -0300

    feat(Gift): adds gift component

---
+++ app/components/Gift/Gift.jsx
@@ -0,0 +1,12 @@
+import styles from "./Gift.module.css";
+
+export default function Gift() {
+  return (
+    <div className={styles.gift}>
+      <div className={styles.top}></div>
+      <div className={styles.body}></div>
+      <div className={styles.tie1}></div>
+      <div className={styles.tie2}></div>
+    </div>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Gift from "./components/Gift/Gift";
+
+export default function Home() {
+  return (
+    <div>
+      <Gift />
+    </div>
+  );
+}
